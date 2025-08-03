418c33a34d0f50cc6dad3c10e5552ec07fd60aad
SDET-Vivek-28 <apurwavivek28@gmail.com>
2025-08-03 22:01:48 +0530

    Fix build: Exclude mobile directory from web deployment using .vercelignore

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  // Exclude mobile directory from build
+  experimental: {
+    excludeDefaultMomentLocales: true,
+  },
 }
 
 module.exports = nextConfig 
